a39cc9efc08b57f0f2b79caa8e5403f2b98bafdb
fusivox-tech <fusivox@gmail.com>
2026-05-30 15:48:54 +0100

    yyyyc

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone', 
-  compiler: {
-    removeConsole: true,
-    reactRemoveProperties: true,
-  },
-  optimizeFonts: true,
-  swcMinify: true,
-  transpilePackages: [],
 };
 
 export default nextConfig;
